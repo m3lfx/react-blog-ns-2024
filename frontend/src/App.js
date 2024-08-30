89acaf02abafb8a3def50e56f9255d871952a05e
rommel dalisay <rommelmdalisay@gmail.com>
2024-08-30 11:36:32 +0800

    navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import Nav from './Nav';
 function App() {
   return (
     <div className="App">
-      <Nav />
+      
       <h1>mern crud</h1>
       
     </div>
--- frontend/src/RoutedApp.js
+++ frontend/src/RoutedApp.js
@@ -2,12 +2,15 @@ import React from 'react'
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
+import Create from './Create';
+import Nav from './Nav';
 const RoutedApp = () => {
     return (
         <Router>
+            <Nav />
             <Routes>
                 <Route path="/" exact="true" element={<App />} />
-                {/* <Route path="/create" exact="true" element={<Create />} /> */}
+                <Route path="/create" exact="true" element={<Create />} />
             </Routes>
         </Router>
     )
