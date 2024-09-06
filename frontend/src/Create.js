c046f01cb3a0bca0292d1f1e89ee62f88ca355cc
rommel dalisay <rommelmdalisay@gmail.com>
2024-09-06 11:24:31 +0800

    create post

--- frontend/src/Create.js
+++ frontend/src/Create.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Nav from './Nav';
 import Title from './Title';
+import axios from 'axios'
+
 const Create = () => {
     const [state, setState] = useState({
         title: '',
@@ -14,13 +16,32 @@ const Create = () => {
 
         setState({ ...state, [name]: event.target.value });
     };
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        // console.table({ title, content, user });
+        axios
+            .post(`${process.env.REACT_APP_API}/post`, { title, content, user })
+            .then(response => {
+                console.log(response);
+                // empty state
+                setState({ ...state, title: '', content: '', user: '' });
+                // show sucess alert
+                alert(`Post titled ${response.data.title} is created`);
+            })
+            .catch(error => {
+                console.log(error.response);
+                alert(error.response.data.error);
+            });
+    };
+
     console.log(state)
     return (
         <div className="container p-5">
             <Title name="create blog post" subTitle="my comment" />
             <h1>CREATE POST</h1>
             <br />
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="form-group">
                     <label className="text-muted">Title</label>
                     <input type="text" className="form-control" placeholder="Post title" required onChange={handleChange('title')} value={title} />
