79cf3ab384d53781bd1885180a9926c6bd477524
rommel dalisay <rommelmdalisay@gmail.com>
2024-09-11 09:39:38 +0800

    login logout

--- frontend/src/Nav.js
+++ frontend/src/Nav.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { getUser, logout } from './helpers';
 
 const Nav = () => {
+    let navigate = useNavigate()
     return (
         <nav>
             <ul className="nav nav-tabs">
@@ -11,9 +13,16 @@ const Nav = () => {
                 <li className="nav-item pr-3 pt-3 pb-3">
                     <Link to="/create">Create</Link>
                 </li>
-                <li className="nav-item ml-auto pr-3 pt-3 pb-3">
+                {getUser() ? <li
+                    onClick={() => logout(() => navigate('/'))}
+                    className="nav-item ml-auto pr-3 pt-3 pb-3"
+                    style={{ cursor: 'pointer' }}
+                >
+                    Logout
+                </li> :  <li className="nav-item ml-auto pr-3 pt-3 pb-3">
                     <Link to="/login">Login</Link>
-                </li>
+                </li> }
+               
             </ul>
         </nav>
     )
